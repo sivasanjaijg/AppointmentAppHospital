b113ca9c92c89de0a69f76463f61b0dea72de6af
sivasanjaijg <121182463+sivasanjaijg@users.noreply.github.com>
2023-01-09 17:27:01 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_health_care_app/config/route.dart';
+import 'package:flutter_health_care_app/theme/theme.dart';
+
+
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       debugShowCheckedModeBanner: false,
+      title: 'Hospital Appointment App',
+      theme:AppTheme.lightTheme,
+      routes: Routes.getRoute(),
+        onGenerateRoute: (settings) => Routes.onGenerateRoute(settings),
+     
+    );
+  }
+}
+
